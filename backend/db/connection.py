7870f8b21081a9c8e250db4e7265ff8c16d81188
soyeongsohn <thdud6265@gmail.com>
2021-11-06 23:15:06 +0900

    create package for db connection

--- backend/db/connection.py
+++ backend/db/connection.py
@@ -1,4 +1,9 @@
 import sqlalchemy
+from pathlib import Path
+import json
+import pandas as pd
+import pymysql
+import os
 
 def db_conn(db_info):
     database_username = db_info['user']
@@ -10,3 +15,19 @@ def db_conn(db_info):
                                                           database_ip, database_name))
 
     return database_connection
+
+
+def get_df(tablename):
+    global n
+    dirpath = Path(__file__).parent.parent.parent
+    with open(os.path.join(dirpath, "db_private.json")) as f:
+        db_info = json.load(f)
+    conn = pymysql.connect(host=db_info['host'], user=db_info['user'], password=db_info['password'], db=db_info['db'])
+    sql = f"SELECT * from {tablename}"
+    df = pd.read_sql(sql, conn)
+
+    if tablename != 'review':
+        df.credit = df.credit.astype(str)
+
+    conn.close()
+    return df
