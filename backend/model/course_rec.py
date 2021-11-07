b6b21dc79978f850845db643f771e1eac1d5181c
soyeongsohn <thdud6265@gmail.com>
2021-11-08 02:21:48 +0900

    recommend top n courses, need to add weight

---
+++ backend/model/course_rec.py
@@ -0,0 +1,37 @@
+from db.db_connection.connection import get_df
+from model.sugang_info import get_sugang_info
+from model.course_scoring import add_sim
+
+
+def top_n(n=10):
+    df = get_df('course_fillna')
+    sugang_info = get_sugang_info()
+    title_list = list(sugang_info['title'])
+    course_sim = sorted(add_sim(title_list), key=lambda x: x[1], reverse=True)
+    rank_idx = [i[0] for i in course_sim]
+    course_2021 = get_df('course_2021')
+    title_2021 = list(course_2021['title'])
+    profname_2021 = list(course_2021['profname'])
+
+    cnt = 0
+    top_idx = []
+    for i in rank_idx:
+        ignore = []
+        if (df['subcode'][i] in list(course_2021['subcode'])) and (df['title'][i] not in title_2021):
+            ignore.append(df['title'][i])
+            ignore.append(course_2021.loc[course_2021.subcode == df['subcode'][i], 'title'].values[0])
+
+        if df['title'][i] in ignore:
+            continue
+
+        elif (df['title'][i] in title_2021) and (df['profname'][i] in profname_2021):
+            if title_2021.index(df['title'][i]) == profname_2021.index(df['profname'][i]):
+                top_idx.append(i)
+                cnt += 1
+        if cnt == n:
+            break
+
+    recommend = df.iloc[top_idx].reset_index(drop=True)
+    recommend['similarity'] = [i[1] for i in course_sim if i[0] in top_idx]
+
+    return recommend
